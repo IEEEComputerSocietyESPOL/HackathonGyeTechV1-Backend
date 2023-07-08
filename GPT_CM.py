ff2a69815c8be7ddb96bd6577be520d07ab3c1e1
Alexito Snow <ganieves@espol.edu.ec>
2023-07-08 14:23:38 -0500

    GPT como generador de contenido: Community Manager

---
+++ GPT_CM.py
@@ -0,0 +1,35 @@
+import openai
+
+api_key = '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'
+openai.api_key = api_key
+
+def generar_respuesta(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=2000  # Ajusta este valor según tus necesidades
+    )
+    generated_text = response.choices[0].text.strip()
+    return generated_text
+
+continuar = "s"
+while continuar != "n":
+    tema = input("Tema: ")
+    tipo_contenido = input("Tipo de contenido: ")
+    nIdeas = input("Cantidad de ideas: ")
+    default_promp = """Como community manager, tu tarea es brindar n ideas de contenido (videos)
+    a realizar a partir de un tema dado y el tipo de contenido que se desea realizar, donde n es el
+    número de ideas que se desean generar.
+    Ejemplo:
+    Tema: El día de la madre
+    Tipo de contenido: Burlesco
+    Cantidad de ideas: 1
+    Idea: Puedes hacer un video sobre el tipo de mamá que no te gustaría tener.
+    Tema: """ + tema + "\nTipo de contenido: " + tipo_contenido + "\nCantidad de ideas: " + nIdeas + "\nIdea: "
+    
+    chatbot_response = generar_respuesta(default_promp)
+    print("Chatbot:", chatbot_response)
+
+    continuar = input("¿Continuar? s/n: ")
+    if continuar.lower() == "n":
+        print("Chatbot: Hasta luego. ¡Gracias por la conversación!")
