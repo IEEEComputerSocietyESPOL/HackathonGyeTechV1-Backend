6c3fee9fd566353c1d9bf2ee00c777894ffe5f91
Alexito Snow <computer@espol.edu.ec>
2023-07-09 09:37:17 -0500

    Update API

--- API_GPT.py
+++ API_GPT.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import openai
 
-app = FastAPI()
-
-api_key = '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'
+# API KEY
+api_key = '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'
 openai.api_key = api_key
 
 class Idea(BaseModel):
@@ -12,6 +11,31 @@ class Idea(BaseModel):
     tipo_contenido: str
     n_ideas: str
 
+@app.route('/',methods=["GET"])
+def index():
+    return "Hello World"
+
+# BODY
+@app.route('/generar-ideas', methods=['POST'])
+def ideas():
+    data = request.json
+    tema = data.get('tema')
+    tipo_contenido = data.get('tipo_contenido')
+    n_ideas = data.get('n_ideas')
+
+    default_prompt = """
+    Como community manager, tu tarea es brindar n ideas de contenido (videos)
+    a realizar a partir de un tema dado y el tipo de contenido que se desea realizar, donde n es el
+    número de ideas que se desean generar.
+    Ejemplo:
+    Tema: {}
+    Tipo de contenido: {}
+    Cantidad de ideas: {}
+    Idea:
+    """.format(tema, tipo_contenido, n_ideas)
+    
+    chatbot_response = generar_respuesta(default_prompt)
+    return jsonify({'ideas_de_contenido': chatbot_response.split("\n")})
 
 def generar_respuesta(prompt):
     response = openai.Completion.create(
@@ -22,19 +46,39 @@ def generar_respuesta(prompt):
     generated_text = response.choices[0].text.strip()
     return generated_text
 
-@app.post("/ideas")
-#def generar_ideas(request: IdeaRequest):
-def generar_ideas(idea: Idea):
-    default_prompt = """Como community manager, tu tarea es brindar n ideas de contenido (videos)
-    a realizar a partir de un tema dado y el tipo de contenido que se desea realizar, donde n es el
-    número de ideas que se desean generar.
-    Ejemplo:
-    Tema: El día de la madre
-    Tipo de contenido: Burlesco
-    Cantidad de ideas: 1
-    Idea: Puedes hacer un video sobre el tipo de mamá que no te gustaría tener.
-    Tema: """ + idea.tema + "\nTipo de contenido: " + idea.tipo_contenido + "\nCantidad de ideas: " + idea.n_ideas + "\nIdea:"
-    
-    ideas = generar_respuesta(default_prompt).split("\n")
-    return {"Ideas": ideas}
-#http://127.0.0.1:8000/docs
+@app.route('/consejero-contenido', methods=['POST'])
+def consejero():
+    data = request.json
+    idea = data.get('Idea')
+    tipo = data.get('Tipo_Contenido')
+    formato = data.get('Formato')
+    default_prompt = """Como Community Manager, experto en redes sociales, edición de videos e imágenes y publicación de contenido, necesito que me brindes sugerencias según el formato (imagen o video) y tipo de contenido que te indique, y las directrices que tengo q usar para hacer la idea de un contenido dada, bajo el formato "Tipo de sugerencia": "descripción de 5 palabras máximo", recuerda limitar la descripcion de la sugerencia a 5 palabras. Ejemplo:
+Idea: Mis gatitos bailando Cha cha chá
+Formato: video
+Tipo de contenido: gracioso
+Sugerencias:
+Música: Los pollitos dicen
+Efectos de sonido: claxson, niños gritando
+Duración estimada: 40 segundos
+Transiciones: No
+Flujo de video (limítate a 5 secuencias máximo): cortos que muestren a los gatos moviéndose, acercamientos y alejamientos
+En el caso de las imágenes:
+Idea: Taller de Docker
+Formato: Imágenes
+Tipo de contenido: informativo
+Sugerencias:
+Cantidad máxima: 4 a 6
+Gama de colores:  Azul, negro
+Color de tipografía: Blanco
+Tipografía: Arial Black
+Sé breve, no digas que estas son solo sugerencias y demás, solo lo que te he pedido
+Idea: {}
+Tipo: {}
+Formato: {}
+Sugerencias:""".format(idea, tipo, formato)
+    chatbot_response = generar_respuesta(default_prompt)
+    return jsonify({'Sugerencias': chatbot_response.split("\n")})
+
+# Ejecucion Flask
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
