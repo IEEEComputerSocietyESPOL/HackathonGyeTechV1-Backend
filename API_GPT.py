71aa19b53f5f66a311fa23b0c0e4096af58b6b62
Alexito Snow <computer@espol.edu.ec>
2023-07-09 09:24:28 -0500

    Creación del consejero de contenido

--- API_GPT.py
+++ API_GPT.py
@@ -1,7 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 import openai
+
+app = FastAPI()
 
 api_key = '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'
 openai.api_key = api_key
+
+class Idea(BaseModel):
+    tema: str
+    tipo_contenido: str
+    n_ideas: str
+
 
 def generar_respuesta(prompt):
     response = openai.Completion.create(
@@ -12,12 +22,10 @@ def generar_respuesta(prompt):
     generated_text = response.choices[0].text.strip()
     return generated_text
 
-continuar = "s"
-while continuar != "n":
-    tema = input("Tema: ")
-    tipo_contenido = input("Tipo de contenido: ")
-    nIdeas = input("Cantidad de ideas: ")
-    default_promp = """Como community manager, tu tarea es brindar n ideas de contenido (videos)
+@app.post("/ideas")
+#def generar_ideas(request: IdeaRequest):
+def generar_ideas(idea: Idea):
+    default_prompt = """Como community manager, tu tarea es brindar n ideas de contenido (videos)
     a realizar a partir de un tema dado y el tipo de contenido que se desea realizar, donde n es el
     número de ideas que se desean generar.
     Ejemplo:
@@ -25,11 +33,8 @@ while continuar != "n":
     Tipo de contenido: Burlesco
     Cantidad de ideas: 1
     Idea: Puedes hacer un video sobre el tipo de mamá que no te gustaría tener.
-    Tema: """ + tema + "\nTipo de contenido: " + tipo_contenido + "\nCantidad de ideas: " + nIdeas + "\nIdea:"
+    Tema: """ + idea.tema + "\nTipo de contenido: " + idea.tipo_contenido + "\nCantidad de ideas: " + idea.n_ideas + "\nIdea:"
     
-    chatbot_response = generar_respuesta(default_promp)
-    print("Ideas de Contenido:\n", chatbot_response)
-
-    continuar = input("¿Continuar? s/n: ")
-    if continuar.lower() == "n":
-        print("Chatbot: Hasta luego. ¡Gracias por la conversación!")
+    ideas = generar_respuesta(default_prompt).split("\n")
+    return {"Ideas": ideas}
+#http://127.0.0.1:8000/docs
